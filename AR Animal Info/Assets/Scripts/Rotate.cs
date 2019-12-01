832c90e0e3983f2d0d4ddf169df25c7a1e971d1d
JPuno <58330993+JPuno@users.noreply.github.com>
2019-12-01 12:35:28 +0800

    First Commit

---
+++ AR Animal Info/Assets/Scripts/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 touchPos;
+    private Quaternion rotationy;
+    private Quaternion rotationx;
+    private float rotatespeedmodifier = 0.1f;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rotationy = Quaternion.Euler(0f, -touch.deltaPosition.x * rotatespeedmodifier, 0f);
+                transform.rotation = rotationy * transform.rotation;
+            }
+            if(touch.phase == TouchPhase.Moved)
+            {
+                rotationx = Quaternion.Euler(touch.deltaPosition.y * rotatespeedmodifier, 0f, 0f);
+                transform.rotation = rotationx * transform.rotation;
+            }
+
+        }
+    }
+
+   
+}
---
+++ AR Animal Info/Assets/Scripts/Zoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    public float zoomSize;
+    public Camera Zoomcamera;
+    
+    void Update()
+    {
+
+        Zoomcamera.orthographicSize = zoomSize;
+       
+    }
+
+    public void AdjustZoom(float newZoomsize)
+    {
+        zoomSize = newZoomsize;
+    }
+}
